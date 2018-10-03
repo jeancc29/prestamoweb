da6f45e3e3edf6b6ac4c7d1bc7737d865442c827
Jean Carlos Contreras <jeancon29@gmail.com>
2018-10-03 10:50:45 -0400

    prueba en el index

--- models/home.php
+++ models/home.php
@@ -1,7 +1,7 @@
 <?php
 $u = new Utilidades();
-// if($_GET['controller']== "home" || $_GET['controller']== "")
-//     $u->verificar_session();
+if($_GET['controller']== "home" || $_GET['controller']== "")
+    $u->verificar_session();
 
 class HomeModel extends Model{
 	public function Index(){
