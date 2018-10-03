975e9800a66d67816bf2feb6cf329ccdb6481a77
Jean Carlos Contreras <jeancon29@gmail.com>
2018-10-03 10:27:26 -0400

    prueba en el index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 //Config
 require('config.php');
 
-//echo $_GET['controller'] . " ";
+echo $_GET['controller'] . " ";
 // echo $_GET['controller']. " ";
 // echo $_GET['action'] . " ";
 // echo $_GET['id'] . " ";
@@ -16,48 +16,46 @@ require('config.php');
 // 	header('location:'.ROOT_PATH ."login");
 //
 //
-require('clases/bootstrap.php');
-require('clases/controllers.php');
-require('clases/models.php');
-require('views/utilidades.php');
-//
-//
-//
-//
-require('controllers/reportes.php');
-require('controllers/amortizacion.php');
-require('controllers/principal.php');
- require('controllers/home.php');
-require('controllers/home2.php');
-require('controllers/home_clientes.php');
-require('controllers/clientes.php');
-require('controllers/usuarios.php');
-require('controllers/personas.php');
- require('controllers/login.php');
-require('controllers/general.php');
-require('controllers/prestamos.php');
-require('controllers/pagos.php');
-//
-require('models/amortizacion.php');
-require('models/reportes.php');
-require('models/principal.php');
- require('models/home.php');
- require('models/home2.php');
-require('models/home_clientes.php');
- require('models/clientes.php');
- require('models/usuarios.php');
- require('models/personas.php');
- require('models/login.php');
-require('models/general.php');
-require('models/prestamos.php');
-require('models/pagos.php');
-//
+// require('clases/bootstrap.php');
+// require('clases/controllers.php');
+// require('clases/models.php');
+// require('views/utilidades.php');
 
-$bootstrap = new Bootstrap($_GET);
-$controller = $bootstrap->createController();
-if($controller){
-	$controller->executeAction();
-}
+
+// require('controllers/reportes.php');
+// require('controllers/amortizacion.php');
+// require('controllers/principal.php');
+//  require('controllers/home.php');
+// require('controllers/home2.php');
+// require('controllers/home_clientes.php');
+// require('controllers/clientes.php');
+// require('controllers/usuarios.php');
+// require('controllers/personas.php');
+//  require('controllers/login.php');
+// require('controllers/general.php');
+// require('controllers/prestamos.php');
+// require('controllers/pagos.php');
+// //
+// require('models/amortizacion.php');
+// require('models/reportes.php');
+// require('models/principal.php');
+//  require('models/home.php');
+//  require('models/home2.php');
+// require('models/home_clientes.php');
+//  require('models/clientes.php');
+//  require('models/usuarios.php');
+//  require('models/personas.php');
+//  require('models/login.php');
+// require('models/general.php');
+// require('models/prestamos.php');
+// require('models/pagos.php');
+// //
+
+// $bootstrap = new Bootstrap($_GET);
+// $controller = $bootstrap->createController();
+// if($controller){
+// 	$controller->executeAction();
+// }
 
 
  ?>
